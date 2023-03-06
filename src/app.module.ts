1fb15df0a720e7dcf89831caf69ad8491a27ded8
OleksandrB93 <barabashsan4ik@gmail.com>
2023-03-06 18:02:58 +0200

    add tests

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CalcController } from './calculator.controller';
+import { CalcController } from './calc/calculator.controller';
 
 @Module({
   imports: [],
---
+++ src/calc/calculator.controller.spec.ts
@@ -0,0 +1,49 @@
+import { CalcController } from './calculator.controller';
+import { Test } from '@nestjs/testing';
+
+describe('CalcController', () => {
+  let calcController: CalcController;
+
+  beforeEach(async () => {
+    const moduleRef = await Test.createTestingModule({
+      controllers: [CalcController],
+    }).compile();
+
+    calcController = moduleRef.get<CalcController>(CalcController);
+  });
+
+  describe('addition', () => {
+    it('return the sum of numbers', () => {
+      expect(calcController.addition('6', '2')).toBe({
+        status: 'OK',
+        data: 8,
+      });
+    });
+  });
+
+  describe('subtract', () => {
+    it('return the difference of numbers', () => {
+      expect(calcController.subtract(6, 2)).toBe({
+        status: 'OK',
+        data: 4,
+      });
+    });
+  });
+
+  describe('multiply', () => {
+    it('return multiplication of numbers', () => {
+      expect(calcController.multiply(6, 2)).toBe({
+        status: 'OK',
+        data: 12,
+      });
+    });
+  });
+  describe('divide', () => {
+    it('return division of numbers', () => {
+      expect(calcController.divide(6, 2)).toBe({
+        status: 'OK',
+        data: 3,
+      });
+    });
+  });
+});
